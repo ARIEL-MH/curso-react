af91fcc79b7dbc60c05ce5db4d99f28667384fef
Ariel <adrielmon4@gmail.com>
2024-02-21 16:20:08 -0500

    manejando eventos

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
 function Navbar() {
+  const [search, setSearch] = useState("");
+
+  function handleInputChange(evt) {
+    setSearch(evt.target.value);
+    //acceder a lo que es usuario esta escribiendo con evt.target.value
+  }
+
   return (
     <div>
       <p>Mi boletera</p>
-      <input type="text" placeholder="Busca tu evento favorito" />
+      <input
+        type="text"
+        placeholder="Busca tu evento favorito"
+        onChange={handleInputChange}
+        value={search}
+      />
     </div>
   );
 }
