3564991a88446bd5e2fc13a505d09829d279b56d
Ariel <adrielmon4@gmail.com>
2024-02-21 16:52:40 -0500

    Event bubbling

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,19 +1,25 @@
 function EventItem({ info, name, id, image, onEventClick }) {
+  //
+
+  const handleSeeMoreClick = (evt) => {
+    evt.stopPropagation();
+    //pilas detener propagacion es una funcion cuando le guarde o de click la propagacion se va tener y el padre no va poder cachar ese evento que fue clicliado a bajo no debemos colisionar eventos del mismo tipo por que no va dar un
+    onEventClick(id);
+  };
   return (
-    <div>
+    //pilas aqui esta haciendo el ejemplo de propagacion das click en el vermas y se hace la propagacion eso es un error
+    <div
+      onClick={
+        //
+        () => {
+          console.log("padre clicleado");
+        }
+      }
+    >
       <img src={image} alt={name} width={200} height={200} />
       <h4>{name}</h4>
       <p>{info}</p>
-      <button
-        onClick={
-          //
-          () => {
-            onEventClick(id);
-          }
-        }
-      >
-        Ver mas
-      </button>
+      <button onClick={handleSeeMoreClick}>Ver mas</button>
     </div>
   );
 }
