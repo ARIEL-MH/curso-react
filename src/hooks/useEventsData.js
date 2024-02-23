662345fe0c94e60420c97c62fed22dede6020081
Ariel <adrielmon4@gmail.com>
2024-02-23 09:56:50 -0500

    Ejemplo usando useRef

--- src/hooks/useEventsData.js
+++ src/hooks/useEventsData.js
@@ -1,14 +1,23 @@
 import eventsJson from "../data/events.json";
-import { useState } from "react";
+import { useEffect, useRef } from "react";
 
 function useEventsData() {
-  const [data, setData] = useState(eventsJson);
+  //ojo si quiero cambiar useState x useRef tengo que preguntamer que erros me van a dar cuando no estamos usando setData
+  const data = useRef([]);
 
-  const {
-    _embedded: { events },
-  } = data;
-
-  return { events };
+  //voy hacer un cargado fictisio despues de 5 segundos
+  useEffect(
+    //informacion de la api
+    () => {
+      setTimeout(() => {
+        data.current = eventsJson;
+      }, 4000);
+      //LOAD API CALL
+    },
+    []
+  );
+  console.log(data.current); // referencia actualizada no vuelve actualizar el componente hooks producen re-render
+  return { events: data.current?._embedded?.events || [] };
 }
 
 export default useEventsData;
