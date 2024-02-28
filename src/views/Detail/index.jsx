47c1af241fd0aa58cc82dc526f7fa246da541a29
Ariel <adrielmon4@gmail.com>
2024-02-28 10:46:25 -0500

    rutas

--- src/views/Detail/index.jsx
+++ src/views/Detail/index.jsx
@@ -1,3 +1,8 @@
-const Detail = () => <div>Detalle</div>;
+import { useParams } from "react-router-dom";
+const Detail = () => {
+  const id = useParams();
+  console.log(id);
+  return <div></div>;
+};
 
 export default Detail;
--- src/views/Profile/index.jsx
+++ src/views/Profile/index.jsx
@@ -3,6 +3,7 @@ const Profile = () => {
   return (
     <div>
       Profile
+      {/* para que se muestre la ruta hijos de children*/}
       <Outlet />
     </div>
   );
