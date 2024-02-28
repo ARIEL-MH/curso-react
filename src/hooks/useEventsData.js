13ccbe4c29c864c360b0eeee63d578e4adbf1089
Ariel <adrielmon4@gmail.com>
2024-02-28 10:55:46 -0500

    como funciona fetch

--- src/hooks/useEventsData.js
+++ src/hooks/useEventsData.js
@@ -1,28 +1,24 @@
-import eventsJson from "../data/events.json";
 import { useEffect, useState } from "react";
 
 function useEventsData() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState();
-  useEffect(
-    //
-    () => {
-      setTimeout(
-        //
-        () => {
-          try {
-            setData(eventsJson);
-            setIsLoading(false);
-          } catch (error) {
-            setError(error);
-          }
-        },
-        4000
-      );
-    },
-    []
-  );
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await fetch(
+          "https://app.ticketmaster.com/discovery/v2/events.json?apikey=GXLc51KXkIk9KT9CqlQnebmD2FDDvW2n&countryCode=MX"
+        );
+        const data = await response.json();
+        setData(data);
+        setIsLoading(false);
+      } catch (e) {
+        setError(e);
+      }
+    };
+    fetchEvents();
+  }, []);
   return {
     //
     events: data._embedded?.events || [],
