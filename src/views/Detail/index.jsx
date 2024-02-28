79f9f64ae6d914055fdd6d5245e26e83992270e2
Ariel <adrielmon4@gmail.com>
2024-02-28 12:59:14 -0500

    Agregando Pantalla de Detalle

--- src/views/Detail/index.jsx
+++ src/views/Detail/index.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
+import styles from "./Detail.module.css";
 const Detail = () => {
   const { eventId } = useParams();
   const [eventData, setEventData] = useState({});
@@ -17,13 +19,60 @@ const Detail = () => {
         setEventData(data);
       } catch (e) {
         setEventData({});
-        setError(error);
+        setError(e);
         setIsLoading(false);
       }
     };
     fetchEventData();
   }, []);
-  return <div>Detalle</div>;
+
+  console.log(eventData);
+  if (isLoading && Object.keys(eventData) === 0) {
+    return <div>Cargando evento...</div>;
+  }
+
+  if (Object.keys(error) > 0) {
+    return <div>Ha ocurrido un error...</div>;
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.mainInfoContainer}>
+        <img
+          src={eventData.images?.[0].url}
+          className={styles.eventImage}
+          alt={eventData.name}
+        />
+        <h4 className={styles.eventName}>{eventData.name}</h4>
+        <p className={styles.infoParagraph}>{eventData.info}</p>
+        <p className={styles.dateParagraph}>
+          {eventData.dates?.start.dateTime
+            ? format(
+                new Date(eventData.dates?.start.dateTime),
+                " d LLLL yyyy H:mm",
+                { locale: es }
+              )
+            : null}
+          hrs
+        </p>
+      </div>
+      <div className={styles.seatInfoContainer}>
+        <h6 className={styles.seatMaptitle}>Mapa del evento</h6>
+        <img
+          src={eventData.seatmap?.staticUrl}
+          alt="Seatmap event"
+          srcset=""
+          width={500}
+        />
+        <p className={styles.pleaseNoteLegend}>{eventData.pleaseNote}</p>
+        <p className={styles.rangoPrecios}>
+          Rango de precios: {eventData.priceRanges?.[0].min}-
+          {eventData.priceRanges?.[0].max} {eventData.priceRanges?.[0].currency}
+        </p>
+      </div>
+      <a href={eventData?.url}>Ir por tus boletos</a>
+    </div>
+  );
 };
 
 export default Detail;
