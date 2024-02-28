021f1e29ece8704d82ce0ab8a3085ca75519bbe8
Ariel <adrielmon4@gmail.com>
2024-02-28 12:09:16 -0500

    Usando fetch en Pantalla de Detalle

--- src/views/Detail/index.jsx
+++ src/views/Detail/index.jsx
@@ -1,8 +1,29 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 const Detail = () => {
-  const id = useParams();
-  console.log(id);
-  return <div></div>;
+  const { eventId } = useParams();
+  const [eventData, setEventData] = useState({});
+  const [error, setError] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  // console.log(eventId);
+  useEffect(() => {
+    const fetchEventData = async () => {
+      try {
+        const response = await fetch(
+          `https://app.ticketmaster.com/discovery/v2/events/${eventId}?apikey=GXLc51KXkIk9KT9CqlQnebmD2FDDvW2n`
+        );
+        const data = await response.json();
+        setEventData(data);
+      } catch (e) {
+        setEventData({});
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+    fetchEventData();
+  }, []);
+  return <div>Detalle</div>;
 };
 
 export default Detail;
