28def3b69109624731515d60c6d5ec22f09800fc
Ariel <adrielmon4@gmail.com>
2024-02-29 18:53:19 -0500

    Creando Tabs

--- src/views/Profile/index.jsx
+++ src/views/Profile/index.jsx
@@ -1,9 +1,39 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
+
+import styles from "./Profile.module.css";
+
 const Profile = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const handleTabClick = (path) => {
+    navigate(`/profile/${path}`);
+  };
+
   return (
-    <div>
-      Profile
-      {/* para que se muestre la ruta hijos de children*/}
+    <div className={styles.cont}>
+      <Link to="/" className={styles.homeLink}>
+        Inicio
+      </Link>
+      <div className={styles.tabsContainer}>
+        <span
+          className={`${pathname.includes("my-info") ? styles.active : ""} ${
+            styles.tab
+          }`}
+          onClick={() => handleTabClick("my-info")}
+          style={{ marginRight: 8 }}
+        >
+          Mi informacion
+        </span>
+        <span
+          className={`${
+            pathname.includes("liked-events") ? styles.active : ""
+          } ${styles.tab}`}
+          onClick={() => handleTabClick("liked-events")}
+        >
+          Eventos Favoritos
+        </span>
+      </div>
       <Outlet />
     </div>
   );
